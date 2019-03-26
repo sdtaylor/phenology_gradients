9da18faadbba8d86f5ab70b3137bc0daeb4d0930
Shawn Taylor <shawntaylor@weecology.org>
2019-03-26 14:37:10 -0400

    put clustering option in simulated data generator

--- run_estimators.R
+++ run_estimators.R
@@ -21,29 +21,6 @@ box_sizes = c(0.3, 0.6)
 edge_buffers = c(0, 0.1, 0.2)
 
 all_estimates_file = 'results/all_estimates.csv'
-###################################################
-###################################################
-generate_clustered_points = function(n, uniform_random_percent=0.2){
-  uniform_n = floor(n * uniform_random_percent)
-  clumped_n = n - uniform_n
-  point_pattern = spatstat::rThomas(5, 0.05, clumped_n*1.5)
-  
-  # Spatstat will generate a random number of points, so from
-  # those choose the n points requested.
-  number_of_random_points = length(point_pattern$x)
-  if(number_of_random_points < clumped_n){
-    # If not enough random points were generated try again
-    return(generate_clustered_points(clumped_n, uniform_random_percent = uniform_random_percent))
-  } else {
-    uniform_x = runif(n=uniform_n, min=0, max=1)
-    uniform_y = runif(n=uniform_n, min=0, max=1)
-    random_selection = sample(1:number_of_random_points, size = clumped_n,replace = FALSE)
-    x = c(point_pattern$x[random_selection], uniform_x)
-    y = c(point_pattern$y[random_selection], uniform_y)
-    return(tibble(x=x, y=y))
-  }
-}
-
 
 #####################################################
 # extract the slope estimates from a SpatialGridModel
@@ -92,24 +69,12 @@ all_estimates = foreach(iteration_i = 1:nrow(all_parameter_combos), .combine = b
   do_clustering = all_parameter_combos$clustering[iteration_i]
   bootstrap_i = all_parameter_combos$bootstrap_i[iteration_i]
 
-  # The flowering sampler will generate completely random, and spatially
-  # uniform, points by default. If doing clustering this will pass in
-  # clusted points
-  if(do_clustering){
-    clustered_points = generate_clustered_points(this_sample_size)
-    x = clustered_points$x
-    y = clustered_points$y
-  } else {
-    x = NULL
-    y = NULL
-  }
-  
   simulated_sample_data = spatialFloweringSampler(n=this_sample_size,
                                                   distribution_type = 'a',
                                                   start_doy = 90,
                                                   flowering_length = this_length,
                                                   flowering_gradient = this_gradient,
-                                                  x=x,y=y)
+                                                  clustering=do_clustering)
 
   for(this_n_boxes in n_boxess){
     for(this_box_size in box_sizes){
