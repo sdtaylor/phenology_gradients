06204b61d1863aa54aeead512d06d3776b29ca83
Shawn Taylor <shawntaylor@weecology.org>
2019-03-31 13:44:59 -0400

    save raw models
    instead of doing the gradient estimate, just
    save the full model for eval later.

--- run_estimators.R
+++ run_estimators.R
@@ -6,22 +6,31 @@ source('spatial_grid_estimator.R')
 ###################################################
 # Config
 
-n_cores = 10
+n_cores = 2
 
 # Sampling parameters
 sample_sizes = c(300, 600)
-flowering_lengths = c(15,30)
-flowering_gradients = c(10/0.1, 20/0.1, 30/0.1)
-clustering = c(TRUE,FALSE)
-n_bootstrap = 2
+flowering_lengths = c(30)
+flowering_gradients = c(10/0.1, 30/0.1)
+clustering = c(TRUE)
+n_bootstrap = 4
 
 # Spatial model parameters
-n_boxess = c(200)
-box_sizes = c(0.3, 0.6)
+#n_boxess = c(200)
+box_sizes = c(0.2, 0.4)
 edge_buffers = c(0, 0.1, 0.2)
 
-all_estimates_file = 'results/all_estimates.csv'
+#all_estimates_file = 'results/all_estimates.csv'
+current_time =  format(Sys.time(),'%Y-%m-%d-%H%M')
+model_dir = paste0('results/models/',current_time,'/')
+dir.create(model_dir, recursive = T)
 
+model_run_note = "
+running for the first time w/ the grid estimator using stratified random sampling.
+not testing non-clustered samples.
+"
+
+cat(model_run_note, file=paste0(model_dir,'notes.txt'))
 #####################################################
 # extract the slope estimates from a SpatialGridModel
 get_slope_estimates = function(spatial_model,
@@ -55,11 +64,17 @@ all_parameter_combos = expand.grid(sample_size = sample_sizes,
                                    flowering_lengths = flowering_lengths,
                                    flowering_gradients = flowering_gradients,
                                    clustering = clustering,
+                                   
+                                   box_size = box_sizes,
+                                   edge_buffer = edge_buffers,
+                                   
                                    bootstrap_i = 1:n_bootstrap)
 
+all_parameter_combos$model_id = 1:nrow(all_parameter_combos)
+all_parameter_combos$model_filename = paste0(model_dir,'estimator_',all_parameter_combos$model_id,'.rds')
 
-all_estimates = foreach(iteration_i = 1:nrow(all_parameter_combos), .combine = bind_rows, .errorhandling = 'remove', .packages = c('dplyr','tidyr','broom')) %dopar% {
-#all_estimates = foreach(iteration_i = 1:nrow(all_parameter_combos), .combine = bind_rows, .errorhandling = 'remove', .packages = c('dplyr','tidyr','broom')) %do% {
+#all_estimates = foreach(iteration_i = 1:nrow(all_parameter_combos), .combine = bind_rows, .errorhandling = 'remove', .packages = c('dplyr','tidyr','broom')) %dopar% {
+all_estimates = foreach(iteration_i = 1:nrow(all_parameter_combos), .combine = bind_rows, .errorhandling = 'remove', .packages = c('dplyr','tidyr','broom')) %do% {
     
   this_iteration_estimates = tibble()
   
@@ -67,8 +82,14 @@ all_estimates = foreach(iteration_i = 1:nrow(all_parameter_combos), .combine = b
   this_length = all_parameter_combos$flowering_lengths[iteration_i]
   this_gradient = all_parameter_combos$flowering_gradients[iteration_i]
   do_clustering = all_parameter_combos$clustering[iteration_i]
+
+  this_box_size = all_parameter_combos$box_size[iteration_i]
+  this_edge_buffer = all_parameter_combos$edge_buffer[iteration_i]
+  
   bootstrap_i = all_parameter_combos$bootstrap_i[iteration_i]
 
+  model_filename = all_parameter_combos$model_filename[iteration_i]
+  
   simulated_sample_data = spatialFloweringSampler(n=this_sample_size,
                                                   distribution_type = 'a',
                                                   start_doy = 90,
@@ -76,34 +97,31 @@ all_estimates = foreach(iteration_i = 1:nrow(all_parameter_combos), .combine = b
                                                   flowering_gradient = this_gradient,
                                                   clustering=do_clustering)
 
-  for(this_n_boxes in n_boxess){
-    for(this_box_size in box_sizes){
-      for(this_edge_buffer in edge_buffers){
-      
-        # Model estimating onset, peak, and end of flowering across space
-        spatial_estimator = PhenologyGridEstimator(doy_points = simulated_sample_data,
-                                                   n_boxes = this_n_boxes,
-                                                   max_box_size = this_box_size, min_box_size = this_box_size,
-                                                   edge_buffer = this_edge_buffer)
-        
-        slope_estimates = get_slope_estimates(spatial_estimator,
-                                              prediction_grid = prediction_grid)
-        
-        slope_estimates$sample_size = this_sample_size
-        slope_estimates$flowering_length = this_length
-        slope_estimates$flowering_gradient = this_gradient
-        slope_estimates$clustering = do_clustering
-        slope_estimates$bootstrap_i = bootstrap_i
-        slope_estimates$n_boxes = this_n_boxes
-        slope_estimates$box_size = this_box_size
-        slope_estimates$edge_buffer = this_edge_buffer
-        
-        this_iteration_estimates = this_iteration_estimates %>%
-          bind_rows(slope_estimates)
-      }
-      }
-  }
+
+  # Model estimating onset, peak, and end of flowering across space
+  spatial_estimator = PhenologyGridEstimator(doy_points = simulated_sample_data,
+                                             boxes_per_stratum = 2,
+                                             box_size = this_box_size,
+                                             edge_buffer = this_edge_buffer)
+  
+  write_rds(spatial_estimator, model_filename)
+  # slope_estimates = get_slope_estimates(spatial_estimator,
+  #                                       prediction_grid = prediction_grid)
+  # 
+  # slope_estimates$sample_size = this_sample_size
+  # slope_estimates$flowering_length = this_length
+  # slope_estimates$flowering_gradient = this_gradient
+  # slope_estimates$clustering = do_clustering
+  # slope_estimates$bootstrap_i = bootstrap_i
+  # slope_estimates$n_boxes = this_n_boxes
+  # slope_estimates$box_size = this_box_size
+  # slope_estimates$edge_buffer = this_edge_buffer
+  # 
+  # this_iteration_estimates = this_iteration_estimates %>%
+  #   bind_rows(slope_estimates)
+  
   return(this_iteration_estimates)
 }
 
-write_csv(all_estimates, all_estimates_file)
+#write_csv(all_estimates, all_estimates_file)
+write_csv(all_parameter_combos, paste0(model_dir,'estimator_metadata.csv'))
