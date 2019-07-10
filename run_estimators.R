91fd1e7cb1d13fe56b48ee8cace77cba75931acc
Shawn <shawntaylor@weecology.org>
2019-07-10 13:44:03 -0400

    use a config file for all the various parameters

--- run_estimators.R
+++ run_estimators.R
@@ -2,58 +2,10 @@ library(tidyverse)
 source('flowering_gradient_generator.R')
 source('spatial_estimators.R')
 
+source('config.R')
 
 ###################################################
 # Config
-
-n_cores = 2
-n_bootstrap = 1
-
-###################################
-# underlying phenology parameters
-###################################
-flowering_lengths = c(15,30)
-flowering_gradients = c(
-  # slope of lm(sos~latitude) from Melaas et al. 2018 / scale  of simulated scale
-  3.36/0.1, 
-  # Half the above, representing a more uniform spatial gradient
-  1.68/0.1)
-spatial_gradient_types = c('linear','non-linear')
-###################################
-# sampling parameters
-###################################
-sample_sizes = c(300, 600)
-clustering = c(TRUE, FALSE)
-
-########################################
-# Spatial model parameters
-######################################
-#n_boxess = c(200)
-weibull_model_parameters = expand.grid(
-  box_size = c(0.2, 0.4),
-  edge_buffer = c(0, 0.1, 0.2)
-)
-weibull_model_parameters$model_id = 1:nrow(weibull_model_parameters)
-
-# bi-squared weighted distance (bisq) model
-bisq_model_parameters = expand.grid(
-  b_distance = c(0.2, 0.5, 0.75, 1.0)
-)
-bisq_model_parameters$model_id = 1:nrow(bisq_model_parameters)
-
-
-# inverse weighted distance (idw) model
-idw_model_parameters = expand.grid(
-  idw_power = c(1,2,3)
-)
-idw_model_parameters$model_id = 1:nrow(idw_model_parameters)
-
-# # linear model 
-# linear_model_parameters = expand.grid(
-#   quantile = c(0.9, 0.95, 0.99)
-# )
-# linear_model_parameters$model_id = 1:nrow(linear_model_parameters)
-# 
 
 #all_estimates_file = 'results/all_estimates.csv'
 current_time =  format(Sys.time(),'%Y-%m-%d-%H%M')
