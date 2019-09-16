0fe124b664122a436a155bc15c8ef8e24bc8c561
Shawn Taylor <shawntaylor@weecology.org>
2019-09-16 14:49:26 -0400

    cleanup col names
    drop open_flowers for open_flower
    drop scientific_name

--- inat_data_prep/compile_annotations.R
+++ inat_data_prep/compile_annotations.R
@@ -4,13 +4,14 @@ source('config.R')
 # Combine the direction annotations from ImageAnt with the full iNaturalist metadata
 # to get the iNat data used for analysis. 
 inat_metadata = read_csv(paste0(data_folder, 'inaturalist_metadata.csv'), col_types = cols('inat_id'='c')) %>%
-  mutate(species = word(scientific_name, 1,2))
+  mutate(species = word(scientific_name, 1,2)) %>%
+  select(-scientific_name)
 
 annotations = read_csv('inat_data_prep/imageant_sessions/inat_rudbeckia.csv', col_types = cols('open_flowers'='c','flowers'='c')) %>%
   bind_rows(read_csv('inat_data_prep/imageant_sessions/inat_maianthemum.csv', col_types = cols('open_flowers'='c','flowers'='c'))) %>%
-  select(-time) %>%
   filter(open_flowers != 'U') %>%
   mutate(open_flower = as.numeric(open_flowers)) %>%
+  select(-time, -open_flowers) %>%
   mutate(inat_id = tools::file_path_sans_ext(file))
 
 inat_data = inat_metadata %>%
