b827d1287f1be25338f96a67411c7b44f5f17cf2
Shawn Taylor <shawntaylor@weecology.org>
2019-09-18 16:43:56 -0400

    finalize phenocam sites

--- phenocam_site_explore.R
+++ phenocam_site_explore.R
@@ -23,12 +23,12 @@ basemap = map_data('state')
 
 ggplot() + 
   geom_polygon(data = basemap, aes(x=long, y = lat, group = group), fill=NA, color='black', size=1.5) +
-  geom_point(data=forest_sites, aes(x=lon, y=lat), size=2.5) + 
-  geom_label_repel(data=forest_sites, aes(x=lon, y=lat,label=site), size=2.5) + 
+  geom_point(data=grassland_sites, aes(x=lon, y=lat), size=2.5) + 
+  geom_label_repel(data=grassland_sites, aes(x=lon, y=lat,label=site), size=2.5) + 
   theme_bw() +
   #ylim(35,55)+
   #xlim(-100,-65)+
-  #coord_fixed(1.3, xlim=c(-120,-65), ylim=c(25,70)) +  
+  coord_fixed(1.3, xlim=c(-120,-65), ylim=c(25,70)) +  
   theme(panel.background = element_rect(fill='white'),
         axis.text = element_text(size=25),
         axis.title = element_text(size=30)) +
@@ -39,26 +39,24 @@ ggplot() +
 # 'tuckerprairie' and 'twosfpr' also look good but they dont start till mid 2018
 #  Notes on ROI's.
 # marena: single ROI of grass
-# kansas: single ROI of grass
 # konza: singel ROI of grass
 # uiefprairie: single ROI of grass
 # ninemileprairie: DB_1000 and DB_2000 are trees, GR_1000 and  GR_2000 are the same grass patch
 # oakville: single ROI of grass
-# sweetbriargrass: single ROI of grass
+# sweetbriargrass: single ROI of grass -Nevermind it's only listed as AG.
 # harvardfarmnorth: hmm, 2 ROIS, says they're different but they look the same. use GR_1000
-selected_gr_sites = c('marena','kansas','konza','uiefprairie','ninemileprairie','oakville','sweetbriargrass','harvardfarmnorth')
+selected_gr_sites = c('marena','konza','uiefprairie','ninemileprairie','oakville','harvardfarmnorth')
 
 # forest sites I chose by hand
-# there's a *bunch*, so inital filter is within latitude (35,55), longitude (-100,-65) and has tart dates < 2015 and
+# there's a *bunch*, so inital filter is within latitude (35,55), longitude (-100,-65) and has start dates < 2015 and
 # end date > 2019 (ie. up to the present).
 # Notes on ROI's. Pick 1000 in all forest cases. 
 # shiningrock: all 3 ROIs (1000,2000,3000) are of the same general area. suspect they're changing from moving camera
 # harvard: all 2 ROIs (0001, 1000) are of same areas. 
 # proctor: single ROI of trees
-# sanford: single ROI of trees
 # willowcreek: single ROI of trees (and sensor)
 # boundarrywaters: all 4 ROIs (1000, 2000, 3000, 4000) are of individual trees, just pick the 1st. 
 # shalehillsczo: both are different areas of trees, very similar. 
-selected_forest_sites = c('shiningrock','harvard','proctor','sanford','willowcreek','boundarrywaters','shalehillsczo')
+selected_forest_sites = c('shiningrock','harvard','proctor','willowcreek','boundarywaters','shalehillsczo')
 selected_sites_info = phenocam_sites %>%
   filter(site %in% selected_sites)
--- process_phenocam_data.R
+++ process_phenocam_data.R
@@ -1,18 +1,19 @@
 library(phenocamr)
+library(tidyverse)
 
 phenocam_site_info = phenocamr::list_sites() %>%
   select(site, lat,lon)
 
 selected_sites = bind_rows(
-  tibble(site_name= c('marena','kansas','konza','uiefprairie','ninemileprairie','oakville','sweetbriargrass','harvardfarmnorth'),
+  tibble(site_name= c('marena','konza','uiefprairie','ninemileprairie','oakville','harvardfarmnorth'),
          veg_type = 'GR'),
-  tibble(site_name = c('shiningrock','harvard','proctor','sanford','willowcreek','boundarrywaters','shalehillsczo'),
+  tibble(site_name = c('shiningrock','harvard','proctor','willowcreek','boundarywaters','shalehillsczo'),
          veg_type = 'DB')
 )
 
 # for(i in 1:nrow(selected_sites)){
 #   try(
-#   download_phenocam(site = selected_sites$site_name[i],
+#   download_phenocam(site = paste0(selected_sites$site_name[i],'$'),
 #                     veg_type = selected_sites$veg_type[i],
 #                     roi_id = 1000,
 #                     frequency = 3,
@@ -21,7 +22,31 @@ selected_sites = bind_rows(
 #   )
 # }
 
-
+# All ROI's are = to 1000, except for these which have some others
+# download_phenocam(site = 'ninemileprairie$',
+#                   veg_type = 'GR',
+#                   roi_id = 2000,
+#                   frequency = 3,
+#                   phenophase = T,
+#                   out_dir = 'data/phenocam_site_data/')
+# download_phenocam(site = 'boundarywaters$',
+#                   veg_type = 'DB',
+#                   roi_id = 4000,
+#                   frequency = 3,
+#                   phenophase = T,
+#                   out_dir = 'data/phenocam_site_data/')
+# download_phenocam(site = 'shiningrock$',
+#                   veg_type = 'DB',
+#                   roi_id = 3000,
+#                   frequency = 3,
+#                   phenophase = T,
+#                   out_dir = 'data/phenocam_site_data/')
+# download_phenocam(site = 'shalehillsczo$',
+#                   veg_type = 'DB',
+#                   roi_id = 2000,
+#                   frequency = 3,
+#                   phenophase = T,
+#                   out_dir = 'data/phenocam_site_data/')
 #############################
 # process the phenocamr derived transition dates
 
@@ -38,5 +63,9 @@ dates = purrr::map_df(transition_date_files, read_transition_file) %>%
   left_join(phenocam_site_info, by='site') %>%
   filter(year>=2016)
 
+# In 2019 in Konza the low estimate was Dec 13, 2018. So it needs to be a negative DOY to be
+# put in the figure correctly. 
+dates = dates %>%
+  mutate(doy_low = ifelse(site=='konza' & year==2019, -18, doy_low))
 
 write_csv(dates, 'data/phenocam_transition_dates.csv')
