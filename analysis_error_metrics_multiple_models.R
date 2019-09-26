e31ca3e82a46aec8d7b4305c48c9c7240526994e
Shawn <shawntaylor@weecology.org>
2019-09-26 16:56:51 -0400

    supp figures showing best weibul parameters

--- analysis_error_metrics_multiple_models.R
+++ analysis_error_metrics_multiple_models.R
@@ -135,55 +135,97 @@ ggplot(sampling_error_means, aes(x=sample_size, y=rmse, color=model)) +
         legend.justification = 'center') + 
   labs(x='Sample Size', y='RMSE', color='')
 
-#################################################
-# Large supplement figure which has boxplots for *all* simulation combinations
-library(kableExtra)
-large_plot_errors = model_errors %>%
-  group_by(model, flowering_lengths, flowering_gradients, spatial_gradient_types, clustering, sample_size) %>%
-  summarise(rmse = mean(rmse)) %>%
-  ungroup() 
-  mutate(flowering_lengths_text = case_when(
-    flowering_gradients == 16.8 ~ 'Weak Gradient (16.8 days)',
-    flowering_gradients == 33.6 ~ 'Moderage Gradient (33.6 days)',
-    flowering_gradients == 67.2 ~ 'Strong Gradient (67.2 days)')) 
+################################################
+# Supplemental figures
+#
+# Same plots as above, but only show the weibull errors,
+# and have labels showing the weibull grid parameters which generated those errors.
+################################################
 
-# large_plot_errors = large_plot_errors %>%
-#   filter(flowering_lengths %in% c(15,45),
-#          sample_size %in% c(150,600,1200))
+#####
+# The best weibul grid parameters with regard to flowering length, gradient type & strength
 
+underlying_phenology_best_model_parameters = model_avg_errors %>%
+  filter(model == 'Weibull Grid') %>%
+  filter(sample_size == 300, clustering == T) %>%
+  group_by(model, flowering_lengths, flowering_gradients, spatial_gradient_types) %>%
+  top_n(2, -rmse) %>%
+  ungroup()  %>%
+  left_join(weibull_model_parameters, by='model_id') %>%
+  mutate(weibull_label = paste('Box size:',box_size,
+                               '\n# Boxes:',num_boxes,
+                               '\nGrid Size:',stratum_size,
+                               '\nRMSE:',round(rmse,2),
+                               '\nBias:',round(bias,2),
+                               '\n% NA:',round(percent_na,2))) %>%
+  mutate(label_y = ifelse(flowering_lengths%%10==0, 'on_top','on_bottom')) %>%
+  mutate(flowering_gradients = round(flowering_gradients,1)) %>% # round these so it joings correctly in the next line
+  left_join(phenology_error_facet_labels, by=c('spatial_gradient_types','flowering_gradients'))
 
-x = large_plot_errors %>%
-  mutate(rmse = round(rmse, 2)) %>%
-  spread(model, rmse) %>%
-  select(spatial_gradient_types, flowering_gradients, flowering_lengths, clustering, sample_size, BiSquared, IDW, `Linear Model`,`Weibull Grid`) %>%
-  arrange(spatial_gradient_types, flowering_gradients, flowering_lengths, clustering, sample_size, BiSquared, IDW, `Linear Model`,`Weibull Grid`)
+ggplot(underlying_phenology_best_model_parameters,aes(y=rmse, x=flowering_lengths)) +
+  geom_label_repel(data=filter(underlying_phenology_best_model_parameters, label_y=='on_top'),
+                   aes(label=weibull_label), size=2.5,
+                   direction = 'x', nudge_y=25) + 
+  geom_label_repel(data=filter(underlying_phenology_best_model_parameters, label_y=='on_bottom'),
+                   aes(label=weibull_label), size=2.5,
+                   direction = 'x', nudge_y = -25) + 
+  geom_point( size=3) + 
+  scale_x_continuous(breaks=c(15,30,45,60), labels = c(15,30,45,60), limits=c(0,70),
+                     minor_breaks = c()) + 
+  ylim(-45,50) + 
+  #coord_cartesian(ylim=c(0,16)) + 
+  facet_wrap(~facet_label, ncol=2) +
+  theme_bw(base_size = 20) +
+  theme(legend.position = 'none',
+        legend.title = element_blank(),
+        legend.background = element_rect(color='black', size=0.5),
+        legend.key.width = unit(20,'mm'),
+        axis.text = element_text(size=18),
+        strip.text = element_text(size=20, hjust = 0),
+        axis.title = element_text(size=20)) + 
+  labs(x='Flowering Length (Days)', y='RMSE', color='', linetype='')
 
-kable(x, 'latex') %>%
-  collapse_rows(columns = 1:4, valign = 'top')
+############
+# The best parameters with regard to sample size and clustering/no clustering
+sampling_scenario_best_model_parameters = model_avg_errors %>%
+  filter(model == 'Weibull Grid') %>%
+  filter(round(flowering_gradients,1) == 33.6, flowering_lengths == 30) %>%
+  group_by(model, spatial_gradient_types, sample_size, clustering) %>%
+  top_n(2, -rmse) %>%
+  ungroup() %>%
+  left_join(weibull_model_parameters, by='model_id') %>%
+  mutate(weibull_label = paste('Box size:',box_size,
+                               '\n# Boxes:',num_boxes,
+                               '\nGrid Size:',stratum_size,
+                               '\nRMSE:',round(rmse,2),
+                               '\nBias:',round(bias,2),
+                               '\n% NA:',round(percent_na,2))) %>%
+  left_join(sampling_error_facet_labels, by=c('spatial_gradient_types','clustering')) %>%
+  mutate(facet_label = forcats::fct_reorder(facet_label, facet_order)) %>%
+  mutate(label_y = ifelse(sample_size %in% c(150,600), 'on_top','on_bottom'))
 
-ggplot(model_errors, aes(x=))
-
-###################################
-# Below is a method to make the plots using a random forest based error models, they end up
-# equivalent to just taking the means of all errors like above. 
-# model_errors$model = as.factor(model_errors$model)
-# model_errors$spatial_gradient_types = as.factor(model_errors$spatial_gradient_types)
-# model_errors$clustering = as.factor(model_errors$clustering)
-# error_model = randomForest(rmse ~ model + sample_size + clustering + flowering_lengths + flowering_gradients + spatial_gradient_types ,
-#                            data=model_errors)
-# 
-# pdp_phenology_data = partial(error_model, pred.var = c('model','flowering_lengths','flowering_gradients','spatial_gradient_types'), n.trees=500) %>%
-#   as_tibble()
-# pdp_phenology_facet_labels = tribble(
-#   ~spatial_gradient_types, ~flowering_gradients, ~facet_label,~facet_order,
-#   'linear',16.8, 'Weak Linear Gradient', 1, 
-#   'non-linear',16.8, 'Weak Non-Linear Gradient', 2,
-#   'linear',33.6, 'Moderate Linear Gradient', 3, 
-#   'non-linear',33.6, 'Moderate Non-Linear Gradient', 4, 
-#   'linear',67.2, 'Strong Linear Gradient', 5, 
-#   'non-linear',67.2, 'Strong Non-Linear Gradient', 6
-# )
-# x = pdp_phenology_data %>%
-#   mutate(flowering_gradients = round(flowering_gradients,1)) %>%
-#   left_join(pdp_phenology_facet_labels, by=c('spatial_gradient_types','flowering_gradients')) %>%
-#   mutate(facet_label = forcats::fct_reorder(facet_label, facet_order))
+ggplot(sampling_scenario_best_model_parameters,aes(x=sample_size, y=rmse, color=model)) + 
+    geom_point(size=3) +
+  geom_label_repel(data = filter(sampling_scenario_best_model_parameters, label_y=='on_top'),
+                   aes(label = weibull_label),size=3, 
+                   vjust=0, direction = 'x', nudge_y = 10) +
+  geom_label_repel(data = filter(sampling_scenario_best_model_parameters, label_y=='on_bottom'),
+                   aes(label = weibull_label),size=3, 
+                   vjust=0, direction = 'x', nudge_y = -15) +
+  scale_color_manual(values = c('black','#0072B2','#E69F00')) +
+  scale_x_continuous(breaks=c(150,300,600,1200), limits = c(-100, 1500),
+                     minor_breaks = c()) + 
+  ylim(-10,15) + 
+  facet_wrap(~facet_label) +
+  theme_bw(base_size = 20) + 
+  theme(axis.text = element_text(size=18),
+        strip.text = element_text(size=20),
+        axis.title = element_text(size=20),
+        legend.position = 'none',
+        legend.key.width = unit(20,'mm'),
+        legend.background = element_rect(color='black'),
+        legend.title = element_blank(),
+        legend.justification = 'center') + 
+  labs(x='Sample Size', y='Bias', color='')
+  
+##############################################################
