5e2e86ab9f5dac2b9ddae3efbed37757c2002198
Shawn <shawntaylor@weecology.org>
2019-10-03 18:15:52 -0400

    update packages used

--- install_packages.R
+++ install_packages.R
@@ -1,3 +1,4 @@
-install.packages(c('spatstat','gstat','tidyverse'))
+install.packages(c('spatstat','gstat','tidyverse','phenocamr','ggrepel'))
 # phest is the package for the weibull estimator
 devtools::install_github("willpearse/phest")
+devtools::install_github("sdtaylor/flowergrids")
